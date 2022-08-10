c4fa8c03731ca0c4cbc6676b97217b4ace973fe7
Hamza Zaheer <malikhamza1619619@gmail.com>
2022-08-10 11:26:47 +0500

    1.12

--- frontend/client/src/App.js
+++ frontend/client/src/App.js
@@ -1,16 +1,13 @@
-import './App.css';
-import {
-  Routes,
-  Route,
-} from "react-router-dom";
-import Navbar from './components/Navbar/Navbar';
-import AdminLogin from './components/AdminLogin/Login'
-import SuperAdminDashboard from './components/SuperAdminDashboard/SuperAdminDashboard'
-import GettingStartedHelp from "./components/Help/pages/GettingStartedHelp"
-import UserRegisterHelp from "./components/Help/pages/UserRegisterHelp"
-import UserLoginHelp from "./components/Help/pages/UserLoginHelp"
-import ContactUs from "./components/ContactUs/ContactUs"
-import AboutUs from "./components/AboutUs/AboutUs"
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import AdminLogin from "./components/AdminLogin/Login";
+import SuperAdminDashboard from "./components/SuperAdminDashboard/SuperAdminDashboard";
+import GettingStartedHelp from "./components/Help/pages/GettingStartedHelp";
+import UserRegisterHelp from "./components/Help/pages/UserRegisterHelp";
+import UserLoginHelp from "./components/Help/pages/UserLoginHelp";
+import ContactUs from "./components/ContactUs/ContactUs";
+import AboutUs from "./components/AboutUs/AboutUs";
 
 import { useContext } from "react";
 import { themeContext } from "./Context";
@@ -19,22 +16,26 @@ function App() {
   const theme = useContext(themeContext);
   const darkMode = theme.state.darkMode;
   return (
-    <div className="App" 
-    style={{
-      background: darkMode ? "black" : "",
-      color: darkMode ? "white" : "",
-    }}
+    <div
+      className="App"
+      style={{
+        background: darkMode ? "black" : "",
+        color: darkMode ? "white" : "",
+      }}
     >
-      <Routes >
+      <Routes>
         <Route path="/" element={<Navbar />} />
-        <Route path="/login" element={< AdminLogin/>} /> 
-        <Route path="/superadmindashboard" element={< SuperAdminDashboard/>} /> 
-        <Route path="/gettingstartedhelp" element={<GettingStartedHelp/>}></Route>
-        <Route path="/userregisterhelp" element={<UserRegisterHelp/>}></Route>
-        <Route path="/userloginhelp" element={<UserLoginHelp/>}></Route>
-        <Route path="/contactus" element={<ContactUs/>}></Route>
-        <Route path="/aboutus" element={<AboutUs/>}></Route>
-    </Routes>
+        <Route path="/login" element={<AdminLogin />} />
+        <Route path="/superadmindashboard" element={<SuperAdminDashboard />} />
+        <Route
+          path="/gettingstartedhelp"
+          element={<GettingStartedHelp />}
+        ></Route>
+        <Route path="/userregisterhelp" element={<UserRegisterHelp />}></Route>
+        <Route path="/userloginhelp" element={<UserLoginHelp />}></Route>
+        <Route path="/contactus" element={<ContactUs />}></Route>
+        <Route path="/aboutus" element={<AboutUs />}></Route>
+      </Routes>
     </div>
   );
 }
--- frontend/client/src/components/SuperAdmincomponents/AddAdmin/AddAdmin.jsx
+++ frontend/client/src/components/SuperAdmincomponents/AddAdmin/AddAdmin.jsx
@@ -1,14 +1,12 @@
-import React from 'react'
-import "./AddAdmin.css"
+import React from "react";
+import "./AddAdmin.css";
 import "../../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Web3 from 'web3';
-import Land from "contracts/Land.json"
-import { useState } from 'react';
+import Web3 from "web3";
+import Land from "contracts/Land.json";
+import { useState } from "react";
 
 let selectedAccount;
 let ContractInstance;
-
-
 
 const AddAdmin = () => {
   const [publicAddress, setPublicAddress] = useState(null);
@@ -21,84 +19,155 @@ const AddAdmin = () => {
   const init = () => {
     let provider = window.ethereum;
     // setIsConnected(true);
-    if (typeof provider !== 'undefined') {
+    if (typeof provider !== "undefined") {
       //metamask is installed
 
-      provider.request({ method: 'eth_requestAccounts' })
-        .then(accounts => {
+      provider
+        .request({ method: "eth_requestAccounts" })
+        .then((accounts) => {
           selectedAccount = accounts[0];
           const web3 = new Web3(provider);
-          ContractInstance = new web3.eth.Contract(Land.abi, Land.networks[5777].address);
+          ContractInstance = new web3.eth.Contract(
+            Land.abi,
+            Land.networks[5777].address
+          );
 
           AddNewAdmin();
-
-        }).catch(err => {
+        })
+        .catch((err) => {
           // setIsConnected(false);
           console.log(err);
           return;
         });
     }
-  }
+  };
 
   //to add new admin
   const AddNewAdmin = async () => {
-
-    await ContractInstance.methods.addAdmin(publicAddress,name,age,designation,city).send({from: selectedAccount});
-    alert("Admin registered Sucessfully.")
+    await ContractInstance.methods
+      .addAdmin(publicAddress, name, age, designation, city)
+      .send({ from: selectedAccount });
+    alert("Admin registered Sucessfully.");
   };
 
-
-
   return (
-    <div className="AddAdmindiv1" >
+    <div className="AddAdmindiv1">
       <div className="AddAdmindiv2">
         <span>Register</span>
-        <span style={{
-          marginBottom: 20
-        }}>New Admin</span>
+        <span
+          style={{
+            marginBottom: 20,
+          }}
+        >
+          New Admin
+        </span>
 
         <form>
-          <div className='container'>
-            <div className='row'>
-              <div className='col-sm-12'>
-                <label for="AdminAddress" class="form-label">Admin Public Address<span style={{color: "#ff0000", marginLeft: "3px"}}>*</span></label>
-                <input type="text" class="form-control" id="AdminAddress" aria-describedby="AdminAddress" required style={{ width: "31.5rem" }}
-                onChange= {e => setPublicAddress(e.target.value)} />
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <label for="AdminAddress" class="form-label">
+                  Admin Public Address
+                  <span style={{ color: "#ff0000", marginLeft: "3px" }}>*</span>
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="AdminAddress"
+                  aria-describedby="AdminAddress"
+                  required
+                  style={{ width: "31.5rem" }}
+                  onChange={(e) => setPublicAddress(e.target.value)}
+                />
               </div>
             </div>
             <br />
-            <div className='row'>
-              <div className='col-sm-6'>
-                <label for="Name" class="form-label">Name<span style={{color: "#ff0000", marginLeft: "3px"}}>*</span></label>
-                <input type="text" class="form-control" id="Name" aria-describedby="Name" required style={{ width: "15rem" }} 
-                onChange= {e => setName(e.target.value)} />
+            <div className="row">
+              <div className="col-sm-6">
+                <label for="Name" class="form-label">
+                  Name
+                  <span style={{ color: "#ff0000", marginLeft: "3px" }}>*</span>
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="Name"
+                  aria-describedby="Name"
+                  required
+                  style={{ width: "15rem" }}
+                  onChange={(e) => setName(e.target.value)}
+                />
               </div>
-              <div className='col-sm-6'>
-                <label for="Age" class="form-label">Age<span style={{color: "#ff0000", marginLeft: "3px"}}>*</span></label>
-                <input type="number" class="form-control" id="Age" aria-describedby="Age" required style={{ width: "15rem" }} 
-                onChange= {e => setAge(e.target.value)} />
+              <div className="col-sm-6">
+                <label for="Age" class="form-label">
+                  Age
+                  <span style={{ color: "#ff0000", marginLeft: "3px" }}>*</span>
+                </label>
+                <input
+                  type="number"
+                  class="form-control"
+                  id="Age"
+                  aria-describedby="Age"
+                  required
+                  style={{ width: "15rem" }}
+                  onChange={(e) => setAge(e.target.value)}
+                />
               </div>
             </div>
             <br />
-            <div className='row'>
-              <div className='col-sm-6'>
-                <label for="Designation" class="form-label">Designation<span style={{color: "#ff0000", marginLeft: "3px"}}>*</span></label>
-                <input type="text" class="form-control" id="Designation" aria-describedby="Designation" required style={{ width: "15rem" }} 
-                onChange= {e => setDesignation(e.target.value)} />
+            <div className="row">
+              <div className="col-sm-6">
+                <label for="Designation" class="form-label">
+                  Designation
+                  <span style={{ color: "#ff0000", marginLeft: "3px" }}>*</span>
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="Designation"
+                  aria-describedby="Designation"
+                  required
+                  style={{ width: "15rem" }}
+                  onChange={(e) => setDesignation(e.target.value)}
+                />
               </div>
-              <div className='col-sm-6'>
-                <label for="City" class="form-label">City<span style={{color: "#ff0000", marginLeft: "3px"}}>*</span></label>
-                <input type="text" class="form-control" id="City" aria-describedby="City" required style={{ width: "15rem" }} 
-                onChange= {e => setCity(e.target.value)} />
+              <div className="col-sm-6">
+                <label for="City" class="form-label">
+                  City
+                  <span style={{ color: "#ff0000", marginLeft: "3px" }}>*</span>
+                </label>
+                <input
+                  type="text"
+                  class="form-control"
+                  id="City"
+                  aria-describedby="City"
+                  required
+                  style={{ width: "15rem" }}
+                  onChange={(e) => setCity(e.target.value)}
+                />
               </div>
             </div>
             <br />
             <div style={{ display: "flex", flexDirection: "column" }}>
-              <button onClick={(event) => {
-                event.preventDefault();
-                {init()}
-              }}
-              type="submit" class="btn btn-primary" style={{ marginTop: "1rem", marginBottom: "5rem", width: "max-content", backgroundColor: "#242D49", borderColor: "#FCA61F" }}>Transfer</button>
+              <button
+                type="submit"
+                class="btn btn-primary"
+                style={{
+                  marginTop: "1rem",
+                  marginBottom: "5rem",
+                  width: "max-content",
+                  backgroundColor: "#242D49",
+                  borderColor: "#FCA61F",
+                }}
+                onClick={(event) => {
+                  event.preventDefault();
+                  {
+                    init();
+                  }
+                }}
+              >
+                Transfer
+              </button>
             </div>
           </div>
         </form>
@@ -110,7 +179,7 @@ const AddAdmin = () => {
         style={{ background: "var(--purple)" }}
       ></div>
     </div>
-  )
-}
+  );
+};
 
-export default AddAdmin
+export default AddAdmin;
